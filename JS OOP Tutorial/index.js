9a4b24c642207aea45e5e8a0fb70db4b05c1a2ca
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2022-01-04 14:26:30 -0500

    view the constructors

--- JS OOP Tutorial/index.js
+++ JS OOP Tutorial/index.js
@@ -3,31 +3,34 @@ function createCircle(radius) {
   return {
     radius,
     draw: function () {
-      console.log("draw:" + radius);
+      console.log("draw 1.0 : " + radius);
     },
   };
 }
-
-const circle = createCircle(4);
-circle.draw();
+const circle1 = createCircle(4);
 
 // Constructor Function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
-    console.log("draw 2.0:" + this.radius);
+    console.log("draw 2.0 : " + this.radius);
   };
 }
+const circle2 = new Circle(3);
 
 // new version of constructor function:
 class NewCircle {
   constructor(radius) {
     this.radius = radius;
     this.draw = function () {
-      console.log("draw 2.0:" + this.radius);
+      console.log("draw 3.0 : " + this.radius);
     };
   }
 }
+const circle3 = new NewCircle(2);
+circle3.draw();
 
-const circle2 = new Circle(3);
-circle2.draw();
+console.log(circle1.constructor);
+console.log(circle2.constructor);
+console.log(circle3.constructor);
+console.log(NewCircle.constructor);
