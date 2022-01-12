7bfde5c1f055ee70bd6aeab700e7d38eb8a7722a
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2022-01-12 18:08:12 -0500

    Add info plate, 02

--- LittleChess/effect/infoDrawings.js
+++ LittleChess/effect/infoDrawings.js
@@ -23,6 +23,22 @@ function drawHPAndAmmoBars(cxt, piece) {
   let hpBarY = 65;
   let ammoBarY = hpBarY + 20;
 
+  let hpText = `[${piece.c_scale} / ${piece.scale}]`;
+  let ammoText = `[${piece.c_ammo} / ${piece.ammo}]`;
+  let hpBarLength, ammoBarLength;
+  if (piece.scale === 1) {
+    hpBarLength = (200 * piece.c_singleHP) / piece.singleHP;
+    hpText = `[${piece.c_singleHP} / ${piece.singleHP}]`;
+  } else {
+    hpBarLength = (200 * piece.c_scale) / piece.scale;
+  }
+  if (piece.ammo === -1) {
+    ammoBarLength = 0;
+    ammoText = "none";
+  } else {
+    ammoBarLength = (200 * piece.c_ammo) / piece.ammo;
+  }
+
   Canvas.drawText(cxt, " Total HP: ", textLeftMostX, hpBarY, "white", 18);
   Canvas.drawText(cxt, "Ammo left: ", textLeftMostX, ammoBarY, "white", 18);
 
@@ -33,18 +49,6 @@ function drawHPAndAmmoBars(cxt, piece) {
   Canvas.drawLine(cxt, barX, hpBarY, barX + 200, hpBarY, "white", 18);
   Canvas.drawLine(cxt, barX, ammoBarY, barX + 200, ammoBarY, "white", 18);
 
-  let hpBarLength, ammoBarLength;
-  if (piece.scale === 1) {
-    hpBarLength = (200 * piece.c_singleHP) / piece.singleHP;
-  } else {
-    hpBarLength = (200 * piece.c_scale) / piece.scale;
-  }
-  if (piece.ammo === -1) {
-    ammoBarLength = 0;
-  } else {
-    ammoBarLength = (200 * piece.c_ammo) / piece.ammo;
-  }
-
   Canvas.drawLine(cxt, barX, hpBarY, barX + hpBarLength, hpBarY, HpColor, 18);
   Canvas.drawLine(
     cxt,
@@ -54,5 +58,15 @@ function drawHPAndAmmoBars(cxt, piece) {
     ammoBarY,
     AmmoColor,
     18
+  );
+
+  Canvas.drawText(cxt, hpText, textLeftMostX + 170, hpBarY + 5, "black", 16);
+  Canvas.drawText(
+    cxt,
+    ammoText,
+    textLeftMostX + 170,
+    ammoBarY + 5,
+    "black",
+    16
   );
 }
