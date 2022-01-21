405c5067002583f85a1253569478b988f55b33f2
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2022-01-20 22:18:19 -0500

    Delete references

--- JS OOP Tutorial/Spearman.js
+++
@@ -1,9 +0,0 @@
-import { Arm } from "/Arm.js";
-
-export class Spearman extends Arm {
-  constructor() {
-    super();
-    this.name = "Spearman";
-    this.scale = 25;
-  }
-}
--- JS OOP Tutorial/Swordman.js
+++
@@ -1,9 +0,0 @@
-import { Arm } from "/Arm.js";
-
-export class Swordman extends Arm {
-  constructor() {
-    super();
-    this.name = "Swordman";
-    this.scale = 30;
-  }
-}
