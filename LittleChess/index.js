a4f5de2b9729bee936a1340ce5968f623eaa94a1
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2022-02-01 14:05:22 -0500

    Landing page completed

--- LittleChess/index.js
+++ LittleChess/index.js
@@ -1,6 +1,6 @@
 let powersForPlayer1 = document.getElementById("powersForPlayer1");
-
 let powersForPlayer2 = document.getElementById("powersForPlayer2");
+let maxCost = document.getElementById("maxCost");
 
 powersForPlayer1.addEventListener("change", (e) => {
   let power1 = powersForPlayer1.value;
@@ -10,4 +10,9 @@ powersForPlayer1.addEventListener("change", (e) => {
 powersForPlayer2.addEventListener("change", (e) => {
   let power2 = powersForPlayer2.value;
   window.localStorage.setItem("power2", power2);
+});
+
+maxCost.addEventListener("change", (e) => {
+  let cost = parseInt(maxCost.value);
+  window.localStorage.setItem("maxCost", cost);
 });
