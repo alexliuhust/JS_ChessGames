0f175f04f3713647622a657c382c3325298a079d
Alexander <aliu@qdti.com>
2026-07-14 16:06:03 -0400

    Can tell if an arm is out of the strict deploy area

--- LittleChess/common/deployMethods.js
+++ LittleChess/common/deployMethods.js
@@ -18,6 +18,7 @@ const maxX = Math.floor(DW / 50);
 const maxY = Math.floor(DH / 50);
 const gX = Math.floor(GW / 50);
 const useMandarin = window.localStorage.getItem("useMandarin") === "true";
+const strictDeploymentArea = window.localStorage.getItem("strictDeploymentArea") === "true";
 const moneyLeftSpan = document.getElementById("moneyLeft");
 const powerSelect = document.getElementById("powers");
 const clearButton = document.getElementById("clear");
@@ -46,6 +47,8 @@ export class Deploy {
     this.imageIndex = -1;
     this.pieceList = [];
     this.moneyLeft = window.localStorage.getItem("maxCost");
+
+    this.areaLimits = {};
 
     this.infoPanel = new InfoPanel(canvasList.info, useMandarin, true, false);
   }
@@ -92,7 +95,6 @@ export class Deploy {
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
       this.infoPanel.updateTraitInfo(x, y);
-      // console.log(`${x.toFixed()}`, `${y.toFixed()}`);
       Canvas.clear(canvasList.info, DIW, DIH);
       this.infoPanel.drawInfoForSelectedPiece(this.arms[this.imageIndex]);
     });
@@ -146,6 +148,11 @@ export class Deploy {
 
       let drawX = Math.floor(x / 50) * 50;
       let drawY = Math.floor(y / 50) * 50;
+
+      if (!this.isArmInStrictArea(drawX, drawY)) {
+        console.log("Outside!!!!");
+      }
+
       let image = this.elems[this.imageIndex];
       Canvas.drawImg(canvasList.piece, image, 0, 0, 50, 50, drawX + 1, drawY + 1, 48, 48);
 
@@ -229,25 +236,43 @@ export class Deploy {
         }
       }
     }
-    let y1 = 250;
+    let y1 = 200;
     let y2 = DH - y1;
     let xOffset = 300;
     let x1 = 0;
     let x2 = 0;
     let color = "";
     if (this.player === 1) {
-      x1 = DW - xOffset;
-      x2 = DW;
+      x1 = DW - xOffset - 100;
+      x2 = DW - 100;
       color = "blue";
     } else {
-      x1 = xOffset;
-      x2 = 0;
+      x1 = xOffset + 100;
+      x2 = 100;
       color = "red";
     }
+
+    this.areaLimits = { x1: x1, y1: y1, x2: x2, y2: y2 };
+
     let weight = 2;
     Canvas.drawLine(canvasList.map, x1, y1, x2, y1, color, weight);
     Canvas.drawLine(canvasList.map, x1, y2, x2, y2, color, weight);
     Canvas.drawLine(canvasList.map, x1, y1, x1, y2, color, weight);
+    Canvas.drawLine(canvasList.map, x2, y1, x2, y2, color, weight);
+  }
+
+  isArmInStrictArea(x, y) {
+    if (!strictDeploymentArea) return true;
+    let { x1, y1, x2, y2 } = this.areaLimits;
+    let xmin = Math.min(x1, x2);
+    let xmax = Math.max(x1, x2);
+    let ymin = Math.min(y1, y2);
+    let ymax = Math.max(y1, y2);
+    x1 = xmin;
+    x2 = xmax;
+    y1 = ymin;
+    y2 = ymax;
+    return x >= x1 && x < x2 && y >= y1 && y < y2;
   }
 
   storeArmInfo() {
@@ -290,7 +315,6 @@ export class Deploy {
 
       this.imageIndex = index;
       this.deployAnArm(drawX, drawY);
-      // this.imageIndex = -1; // Reset safety
     }
   }
 
